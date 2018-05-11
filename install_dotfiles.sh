0ee6f54ea14e8826b503d265b42036af74b6f996
Marco van den Oever <marco@Hercules.local>
2018-05-11 23:23:41 +0200

    changes from Hercules.local on 11-05-2018 @ 23:23:41

--- install_dotfiles.sh
+++ install_dotfiles.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 #
-echo "Deploying dotfiles to ${HOME}"
+echo "Deploying dotfiles to ${HOME} :\n"
 # Make symlinks
-FILES=$(find $(pwd) -maxdepth 1 -type f -name ".*" -not -name "*.swp")
+FILES=$(find $(pwd) -maxdepth 1 -name ".*" -not -name ".git")
 for FILE in $FILES
 do
 	F=$(basename $FILE);
+	echo ".. ${F}"
 	ln -sn $FILE ${HOME}/${F};
 done
-
-ln -sn $(pwd)/.config ${HOME}/.config
