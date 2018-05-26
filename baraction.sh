d272c0fd825de0cdac1d17b63b81f97afc49b5a1
Marco van den Oever <mvandenoever@basenet.nl>
2018-05-26 10:36:02 +0200

    changes from Hercules.local on 26-05-2018 @ 10:36:02

--- baraction.sh
+++ baraction.sh
@@ -2,7 +2,7 @@
 #
 while :; do
 	CPU_LOAD=$(uptime | sed 's/.*: //; s/,//g')
-	MEM_USED=$(free | grep Mem | awk '{printf "%.1f", $3/$2 * 100.0}'
+	MEM_USED=$(free | grep Mem | awk '{printf "%.1f", $3/$2 * 100.0}')
 
 	echo -e "L: ${CPU_LOAD} M: ${MEM_USED}%"
 
