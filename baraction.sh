8b89215c375b4ebc9d5be922171fcf7a793eae83
Marco van den Oever <mvandenoever@basenet.nl>
2018-05-29 13:27:01 +0200

    changes from dev-lin-002 on 29-05-2018 @ 13:27:01

--- baraction.sh
+++ baraction.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 #
 while :; do
+	UPTIME=$(uptime -p | sed 's/up //;')
 	CPU_LOAD=$(uptime | sed 's/.*: //; s/,//g;' | cut -d' ' -f1)
 	CPU_TEMP=$(sensors | grep -oP 'Physical.*?\+\K[0-9.]+')
 	MEM_USED=$(free | grep Mem | awk '{printf "%.1f", $3/$2 * 100.0}')
 
-	echo "L: ${CPU_LOAD} - T: ${CPU_TEMP}C - M: ${MEM_USED}%"
+	echo "U: ${UPTIME} - L: ${CPU_LOAD} - T: ${CPU_TEMP}C - M: ${MEM_USED}%"
 
 	sleep 5
 done
