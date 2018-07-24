571c1d0129d04a8b5add631e5e8a6af2cf561dc4
Marco van den Oever <mvandenoever@basenet.nl>
2018-07-24 09:53:03 +0200

    changes from NL1-LIN-002 on 24-07-2018 @ 09:53:03

--- scripts/update_debian.sh
+++ scripts/update_debian.sh
@@ -9,6 +9,9 @@ RESET='\033[0m'
 # apt-get with options
 APT="apt-get --quiet --assume-yes"
 #
+# Elevate permissions if needed
+[ "$UID" -eq 0 ] || exec sudo "$0" "$@"
+#
 # Update Debian
 echo -e "\n${GREEN}# Updating System ...${RESET}"
 $APT update && $APT upgrade && $APT autoremove && $APT autoclean
@@ -16,10 +19,13 @@ $APT update && $APT upgrade && $APT autoremove && $APT autoclean
 # Update Docker-compose
 if command -v docker-compose >/dev/null; then
 	echo -e "\n${GREEN}# Updating Docker-compose ...${RESET}"
-	COMPOSE_VERSION=`git ls-remote https://github.com/docker/compose | grep refs/tags | grep -oP "[0-9]+\.[0-9][0-9]+\.[0-9]+$" | tail -n 1`
-	echo -e "${YELLOW}Latest Docker-compose version: ${COMPOSE_VERSION}${RESET}"
-	curl -#Lo /usr/local/bin/docker-compose https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-`uname -s`-`uname -m`
-	chmod +x /usr/local/bin/docker-compose
+	CURRENT_VERSION=`docker-compose version | grep -Po "docker-compose version (?:(\d+)\.?){4}" | awk -F ' ' '{print $3}'`
+	AVAILABLE_VERSION=`git ls-remote https://github.com/docker/compose | grep refs/tags | grep -oP "[0-9]+\.[0-9][0-9]+\.[0-9]+$" | tail -n 1`
+	echo -e "${YELLOW}Current: ${CURRENT_VERSION} - Available: ${AVAILABLE_VERSION}${RESET}"
+	if [[ $CURRENT_VERSION != $AVAILABLE_VERSION ]]; then
+		curl -#Lo /usr/local/bin/docker-compose https://github.com/docker/compose/releases/download/${AVAILABLE_VERSION}/docker-compose-`uname -s`-`uname -m`
+		chmod +x /usr/local/bin/docker-compose
+	fi
 fi
 #
 # Update Minikube
