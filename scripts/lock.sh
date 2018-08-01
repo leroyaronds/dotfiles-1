af83096d5ab8b12f083a15e875b3aa5cac4d9277
Marco van den Oever <mvandenoever@basenet.nl>
2018-08-01 14:49:06 +0200

    changes from NL1-LIN-002 on 01-08-2018 @ 14:49:06

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -5,6 +5,6 @@ TMP_BG=/tmp/screenshot.png
 # Take screenshot
 scrot $TMP_BG
 # Pixelate screenshot
-convert $TMP_BG -scale 10% -scale 1000% $TMP_BG
+convert $TMP_BG -scale 10% -scale 1000% -fill black -colorize 25% $TMP_BG
 # Start screen locker
 i3lock -i $TMP_BG -f -n
