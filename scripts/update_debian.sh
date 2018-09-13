561cadfd6b1b018c542075482b626c238db9e2b2
Marco van den Oever <mvandenoever@basenet.nl>
2018-09-13 10:42:13 +0200

    changes from NL1-LIN-002 on 13-09-2018 @ 10:42:13

--- scripts/update_debian.sh
+++ scripts/update_debian.sh
@@ -15,15 +15,3 @@ APT="apt-get --quiet --assume-yes"
 # Update Debian
 echo -e "\n${GREEN}# Updating System ...${RESET}"
 $APT update && $APT upgrade && $APT autoremove && $APT autoclean
-#
-# Update Docker-compose
-if command -v docker-compose >/dev/null; then
-	echo -e "\n${GREEN}# Updating Docker-compose ...${RESET}"
-	CURRENT_VERSION=`docker-compose version | grep -Po "docker-compose version (?:(\d+)\.?){4}" | awk -F ' ' '{print $3}'`
-	AVAILABLE_VERSION=`git ls-remote https://github.com/docker/compose | grep refs/tags | grep -oP "[0-9]+\.[0-9][0-9]+\.[0-9]+$" | tail -n 1`
-	echo -e "${YELLOW}Current: ${CURRENT_VERSION} - Available: ${AVAILABLE_VERSION}${RESET}"
-	if [ $CURRENT_VERSION != $AVAILABLE_VERSION ]; then
-		curl -#Lo /usr/local/bin/docker-compose https://github.com/docker/compose/releases/download/${AVAILABLE_VERSION}/docker-compose-`uname -s`-`uname -m`
-		chmod +x /usr/local/bin/docker-compose
-	fi
-fi
