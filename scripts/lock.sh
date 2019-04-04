dc0999654b2ed0eec59db856ec1d4c1988f20b37
Marco van den Oever <mvandenoever@basenet.nl>
2019-04-04 11:47:39 +0200

    changes from NL1-LIN-002 on 04-04-2019 @ 11:47:39

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 #
-TMP_BG=/tmp/screenshot.png
-#
-# Take screenshot
-scrot $TMP_BG
-# Pixelate screenshot
-convert $TMP_BG -scale 10% -scale 1000% -fill black -colorize 25% $TMP_BG
-# Start screen locker
-i3lock -i $TMP_BG -f -n
+i3lock -c 000000 -f -n
--- scripts/update_debian.sh
+++ scripts/update_debian.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 set -e
 #
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
-RESET='\033[0m'
-#
-# apt-get with options
-APT="apt-get --quiet --assume-yes"
+# Set APT default options
+APT="apt --quiet --assume-yes --no-install-recommends"
 #
 # Elevate permissions if needed
 [ "$UID" -eq 0 ] || exec sudo "$0" "$@"
 #
 # Update Debian
-echo -e "\n${GREEN}# Updating System ...${RESET}"
+printf "# Updating System:"
 $APT update && $APT upgrade && $APT autoremove && $APT autoclean
+printf "# Updating System: Done"
