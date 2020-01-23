c94cc1f9dc827c47c3c3f3d63b77bb71172cec3e
Marco <marco@localhost>
2020-01-23 15:48:44 +0100

    Added battery to i3status

--- scripts/update_system.sh
+++ scripts/update_system.sh
@@ -7,7 +7,6 @@ APT="apt --quiet --assume-yes --no-install-recommends"
 # Elevate permissions if needed
 [ "$UID" -eq 0 ] || exec sudo "$0" "$@"
 #
-# Update Debian
 printf "# Updating System:\n"
 $APT update && $APT upgrade && $APT autoremove && $APT autoclean
 printf "# Updating System: Done\n"
