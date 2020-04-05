403cc76bc8fc1d4dda7b0c703ecbeb84e5c4a90a
Marco <marco@localhost>
2020-04-05 09:12:17 +0200

    Commit from surface on 05-04-2020 @ 09:12:17

--- scripts/install_surface.sh
+++ scripts/install_surface.sh
@@ -30,7 +30,7 @@ $APT autoremove
 $APT autoclean
 
 # Install packages
-$APT install brightnessctl cmus cpufrequtils git gpg i3status kitty libgfshare-bin linux-headers-$(uname -r) mutt qrencode pinentry-curses pinentry-gnome3 resolvconf scdaemon steghide sway swayidle swaylock tomb vim wireguard wl-clipboard wlfreerdp wpasupplicant zbar-tools zsh
+$APT install brightnessctl cmus cpufrequtils git gpg i3status kitty knockd libgfshare-bin linux-headers-$(uname -r) mutt qrencode pinentry-curses pinentry-gnome3 resolvconf scdaemon steghide sway swayidle swaylock tomb vim wireguard wl-clipboard wlfreerdp wpasupplicant zbar-tools zsh
 
 # Create symbolic links to dotfiles
 ln --symbolic ../.gitconfig ~/.gitconfig
