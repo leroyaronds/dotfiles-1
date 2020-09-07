514684b3e89952509c0ace5338f3c51c37989304
Marco <marco@localhost>
2020-09-07 11:32:56 +0200

    Commit from surface on 07-09-2020 @ 11:32:56

--- scripts/install_surface.sh
+++ scripts/install_surface.sh
@@ -102,7 +102,12 @@ gsettings set org.gnome.desktop.interface cursor-theme 'DMZ-White'
 
 # Install Ledger USB detection
 cat <<EOF > /etc/udev/rules.d/20-hw1.rules
+# Nano S
+SUBSYSTEMS=="usb", ATTRS{idVendor}=="2c97", ATTRS{idProduct}=="0001|1000|1001|1002|1003|1004|1005|1006|1007|1008|1009|100a|100b|100c|100d|100e|100f|1010|1011|1012|1013|1014|1015|1016|1017|1018|1019|101a|101b|101c|101d|101e|101f", TAG+="uaccess", TAG+="udev-acl"
+# Nano X
 SUBSYSTEMS=="usb", ATTRS{idVendor}=="2c97", ATTRS{idProduct}=="0004|4000|4001|4002|4003|4004|4005|4006|4007|4008|4009|400a|400b|400c|400d|400e|400f|4010|4011|4012|4013|4014|4015|4016|4017|4018|4019|401a|401b|401c|401d|401e|401f", TAG+="uaccess", TAG+="udev-acl"
+# HI-power
+KERNEL=="hidraw*", SUBSYSTEM=="hidraw", MODE="0660", GROUP="plugdev", ATTRS{idVendor}=="2c97"
 EOF
 udevadm trigger
 udevadm control --reload-rules
