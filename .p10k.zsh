4be88098ec0fb3b0add3f3a9d932817bfd738e88
Marco <marco@localhost>
2020-11-04 20:37:08 +0100

    Commit from surface on 04-11-2020 @ 20:37:08

--- .p10k.zsh
+++ .p10k.zsh
@@ -757,7 +757,7 @@
   # Hide todo when the total number of tasks is zero.
   typeset -g POWERLEVEL9K_TODO_HIDE_ZERO_TOTAL=true
   # Hide todo when the number of tasks after filtering is zero.
-  typeset -g POWERLEVEL9K_TODO_HIDE_ZERO_FILTERED=false
+  typeset -g POWERLEVEL9K_TODO_HIDE_ZERO_FILTERED=true
 
   # Todo format. The following parameters are available within the expansion.
   #
@@ -770,7 +770,7 @@
   #
   # Here 24 is P9K_TODO_FILTERED_TASK_COUNT and 42 is P9K_TODO_TOTAL_TASK_COUNT.
   #
-  # typeset -g POWERLEVEL9K_TODO_CONTENT_EXPANSION='$P9K_TODO_FILTERED_TASK_COUNT'
+  typeset -g POWERLEVEL9K_TODO_CONTENT_EXPANSION='$P9K_TODO_FILTERED_TASK_COUNT'
 
   # Custom icon.
   # typeset -g POWERLEVEL9K_TODO_VISUAL_IDENTIFIER_EXPANSION='⭐'
